7e3e055033e66736365d5f489f97e89310e00b5e
-list <1034137466@qq.com>
2021-07-25 18:18:31 +0800

    SECOND commit IDEA!

--- src/main/java/com/test/git/GitTest.java
+++ src/main/java/com/test/git/GitTest.java
@@ -6,6 +6,6 @@ package com.test.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!!!!!!!!!");
+        System.out.println("hello GGGGGit!!!!!!!!!");
     }
 }

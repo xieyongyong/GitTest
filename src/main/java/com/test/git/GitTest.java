6212a904508339cb5f3a79a2bcd6ff7b2536c1d1
-list <1034137466@qq.com>
2021-07-25 18:19:38 +0800

    SECOND commit IDEA!

--- src/main/java/com/test/git/GitTest.java
+++ src/main/java/com/test/git/GitTest.java
@@ -1,11 +1,12 @@
 package com.test.git;
 
 /**
- * @author 谢勇勇
- * @create 2021/7/25-18:09
+ * @Author 谢勇勇
+ * @Create 2021/7/25-18:09
  */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello GGGGGit!!!!!!!!!");
+        System.out.println("hello Git!!!!!!!!!");
     }
 }

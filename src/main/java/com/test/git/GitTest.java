058d2ebda14ded53695681f2a812e81001ed577d
-list <1034137466@qq.com>
2021-07-25 18:14:58 +0800

    first commit IDEA!

---
+++ src/main/java/com/test/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.test.git;
+
+/**
+ * @author 谢勇勇
+ * @create 2021/7/25-18:09
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!!!!!!!!!");
+    }
+}
